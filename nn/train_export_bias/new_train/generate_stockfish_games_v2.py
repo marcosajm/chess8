38ddf8979951364a1b93e3920d39769495df54b8
marcosajm <ilejbr@gmail.com>
2026-08-20 21:46:33 +0100

    20082026o

--- nn/train_export_bias/new_train/generate_stockfish_games_v2.py
+++ nn/train_export_bias/new_train/generate_stockfish_games_v2.py
@@ -24,7 +24,7 @@ class Config:
     
     # Data generation
     DEPTH = 24
-    NUM_GAMES = 20
+    NUM_GAMES = 2
     MAX_MOVES = 212
     STOCKFISH_PATH = "/usr/games/stockfish"
     
@@ -277,7 +277,7 @@ class OurBotMoveSelector:
 
             # Determine if this should be a best or worst move
             # Pattern: move 1 = best, moves 2-3 = worst, move 4 = best, moves 5-6 = worst, ...
-            if self.move_counter < 5 & self.move_counter % 3 == 0:
+            if (self.move_counter < 7) and (self.move_counter % 2 == 0):
                 # Best move (every 3rd move starting from 1)
                 selected = move_scores[-1]
                 print(f"  🎯 OUR Bot ({side}) playing BEST move (alternating pattern #{self.move_counter})")
